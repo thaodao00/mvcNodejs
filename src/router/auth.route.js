7485ae4ac9b6f00ba8adc397966a414eccfec289
Đào Thị Thu Thảo <91473426+thaodao00@users.noreply.github.com>
2023-08-24 23:37:11 +0700

    fix upload image

--- src/router/auth.route.js
+++ src/router/auth.route.js
@@ -5,12 +5,11 @@ const router = express.Router();
 const authController = require('./../controllers/auth.controller');
 
 
-router.post('/send-activation-email', authController.sendActivationEmail);
-// router.get('/activate', authController.activateAccount);
 
 router.get('/register', authController.register);
 router.post('/register', authController.registerUser);
 
 router.get(['/','/login'], authController.getLoginAdmin);
 router.post(['/', '/login'], authController.loginUser);
+router.get('/logout', authController.logout);
 module.exports = router;
